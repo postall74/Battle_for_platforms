7cf0fc109f02e0e552fe5c51cb4f9023065f7a34
postall_mcs <postall_mcs@mail.ru>
2025-10-01 01:01:27 +0500

    Second solution (part 1)

--- Assets/Scripts/Enemy/States/PatrolState.cs
+++ Assets/Scripts/Enemy/States/PatrolState.cs
@@ -5,7 +5,7 @@ public class PatrolState : EnemyState
     private readonly Transform _leftPatrolPoint;
     private readonly Transform _rightPatrolPoint;
     private bool _isFacingRight;
-    private float _turnBuffer = 0.5f;
+    private float _turnBuffer = 0.1f;
 
     public PatrolState(EnemyStateMachine stateMachine, EnemyMovement movement, Transform transform,
                        Transform leftPatrolPoint, Transform rightPatrolPoint, bool startFacingRight) : base(stateMachine, movement, transform)
@@ -17,28 +17,22 @@ public class PatrolState : EnemyState
 
     public override void Enter()
     {
-        _movement.Move(_isFacingRight ? 1 : -1);
+        float direction = _isFacingRight ? 1 : -1;
+        _movement.Move(direction);
+        _movement.Flip(direction);
+
     }
 
     public override void Update()
     {
-        bool reachedRightPoint = Physics2D.OverlapArea(
-            new Vector2(_rightPatrolPoint.position.x - _turnBuffer, _rightPatrolPoint.position.y - _turnBuffer),
-            new Vector2(_rightPatrolPoint.position.x + _turnBuffer, _rightPatrolPoint.position.y + _turnBuffer)
-        );
-        bool reachedLeftPoint = Physics2D.OverlapArea(
-            new Vector2(_leftPatrolPoint.position.x - _turnBuffer, _leftPatrolPoint.position.y - _turnBuffer),
-            new Vector2(_leftPatrolPoint.position.x + _turnBuffer, _leftPatrolPoint.position.y + _turnBuffer)
-        );
-
-
-        if (_isFacingRight && reachedRightPoint)
+        if (_isFacingRight && _transform.position.x + _turnBuffer >= _rightPatrolPoint.position.x)
             TurnAround(false);
-        else if (!_isFacingRight && reachedLeftPoint)
+        else if (_isFacingRight == false && _transform.position.x - _turnBuffer <= _leftPatrolPoint.position.x)
             TurnAround(true);
 
         float currentDirection = _isFacingRight ? 1 : -1;
         _movement.Move(currentDirection);
+
     }
 
     public override void Exit()
@@ -50,7 +44,15 @@ public class PatrolState : EnemyState
     {
         _isFacingRight = isFacingRight;
         float newDirection = _isFacingRight ? 1 : -1;
+
+        Vector3 newPosition = _transform.position;
+
+        if (_isFacingRight)
+            newPosition.x = Mathf.Min(newPosition.x, _rightPatrolPoint.position.x - _turnBuffer);
+        else
+            newPosition.x = Mathf.Max(newPosition.x, _leftPatrolPoint.position.x + _turnBuffer);
+
+        _transform.position = newPosition;
         _movement.Flip(newDirection);
-        _movement.Stop();
     }
 }
